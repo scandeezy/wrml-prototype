fd508762a6cf2fb1c3f9171be00c7c307089baa9
Brian R. Jackson <brian@jaxzin.com>
2011-11-16 21:22:04 -0500

    Initial unit test for SchemaLoader.

---
+++ src/test/java/org/wrml/proto/schema/SchemaLoader.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright (C) 2011 WRML.org <mark@wrml.org>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.wrml.proto.schema;
+
+/**
+ * A class that can dynamically load a WRML Schema as a Java class.
+ */
+public class SchemaLoader {
+    public Object load(String uri) {
+        // TODO: This is just enough to make the unit test pass
+        return new Object() {
+            public int getExample() {
+                return 0;
+            }
+        };
+    }
+}
